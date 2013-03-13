dba9e755480a5670c751b853cb4b1ddadd412e9a
benhur1999 <benhur1999@users.noreply.github.com>
2013-03-13 10:55:16 +0000

    - fix "first record out of range" generated for empty result sets

--- src/main/java/eu/clarin/sru/server/SRUServer.java
+++ src/main/java/eu/clarin/sru/server/SRUServer.java
@@ -352,9 +352,10 @@ public final class SRUServer {
 
         // check, of startRecord position is greater than total record set
         if ((result.getTotalRecordCount() >= 0) &&
-            (request.getStartRecord() > 0) &&
-            (request.getStartRecord() > result.getTotalRecordCount())) {
-            throw new SRUException(SRUConstants.SRU_FIRST_RECORD_POSITION_OUT_OF_RANGE);
+            (request.getRawStartRecord() > 0) &&
+            (request.getRawStartRecord() > result.getTotalRecordCount())) {
+            throw new SRUException(
+                    SRUConstants.SRU_FIRST_RECORD_POSITION_OUT_OF_RANGE);
         }
 
         try {
