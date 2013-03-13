fa2e8bd9597693524a3324959fbf5fb7d9ae9a14
benhur1999 <benhur1999@users.noreply.github.com>
2013-03-13 10:57:29 +0000

    - tag version 1.4.1

--- src/main/java/eu/clarin/sru/server/SRURequestImpl.java
+++ src/main/java/eu/clarin/sru/server/SRURequestImpl.java
@@ -65,7 +65,7 @@ final class SRURequestImpl implements SRURequest, SRUDiagnosticList {
     private SRURecordPacking recordPacking;
     private CQLNode query;
     private String rawQuery;
-    private int startRecord = DEFAULT_START_RECORD;
+    private int startRecord = -1;
     private int maximumRecords = -1;
     private String recordSchemaName;
     private String recordSchemaIdentifier;
@@ -75,7 +75,7 @@ final class SRURequestImpl implements SRURequest, SRUDiagnosticList {
     private String sortKeys;
     private CQLNode scanClause;
     private String rawScanClause;
-    private int responsePosition = DEFAULT_RESPONSE_POSITION;
+    private int responsePosition = -1;
     private int maximumTerms = -1;
 
     private static enum Parameter {
@@ -410,6 +410,11 @@ final class SRURequestImpl implements SRURequest, SRUDiagnosticList {
     }
 
 
+    int getRawStartRecord() {
+        return startRecord;
+    }
+
+
     int getRawMaximumRecords() {
         return maximumRecords;
     }
@@ -417,6 +422,11 @@ final class SRURequestImpl implements SRURequest, SRUDiagnosticList {
 
     String getRawScanClause() {
         return rawScanClause;
+    }
+
+
+    int getRawResponsePosition() {
+        return responsePosition;
     }
 
 
@@ -492,7 +502,7 @@ final class SRURequestImpl implements SRURequest, SRUDiagnosticList {
 
     @Override
     public int getStartRecord() {
-        return startRecord;
+        return (startRecord > 0) ? startRecord : DEFAULT_START_RECORD;
     }
 
 
@@ -552,7 +562,8 @@ final class SRURequestImpl implements SRURequest, SRUDiagnosticList {
 
     @Override
     public int getResponsePosition() {
-        return responsePosition;
+        return (responsePosition >= 0)
+                ? responsePosition : DEFAULT_RESPONSE_POSITION;
     }
 
 
