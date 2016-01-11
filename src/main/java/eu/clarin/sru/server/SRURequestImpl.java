4fe1551a5ad8328c10afc227e029155e3e89bde8
benhur1999 <benhur1999@users.noreply.github.com>
2016-01-11 17:13:49 +0000

    - remove stray debugging message

--- src/main/java/eu/clarin/sru/server/SRURequestImpl.java
+++ src/main/java/eu/clarin/sru/server/SRURequestImpl.java
@@ -466,7 +466,6 @@ final class SRURequestImpl implements SRURequest, SRUDiagnosticList {
                         }
                         break;
                     case RECORD_PACKING:
-                        logger.debug("XXXX: RECORD PAXCKING: {}", value);
                         if (value.equals(RECORD_PACKING_PACKED)) {
                             recordPacking = SRURecordPacking.PACKED;
                         } else if (value.equals(RECORD_PACKING_UNPACKED)) {
